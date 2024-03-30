57e0028ee086358d89c7149ca864f90ba83c6afa
Iker King <ik@ik-2.local>
2024-03-30 02:45:19 +0200

    9

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -27,7 +27,7 @@ module.exports = {
       patterns: [
         {
           from: paths.public,
-          to: 'assets',
+          to: 'public',
           globOptions: {
             ignore: ['*.DS_Store'],
           },
